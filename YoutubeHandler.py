e08750627a3d16fb922daab62876c421afcbb2a0
Rob <rob.wagner@outlook.com>
2019-01-21 03:10:29 -0500

    Initial commit

---
+++ YoutubeHandler.py
@@ -0,0 +1,26 @@
+import re
+
+
+class YoutubeHandler:
+
+    def get_video_params(self, url):
+        """Takes a standard youtube video URL and returns a dictionary with each URL parameter."""
+        regex_pattern = re.compile(r'(?<=\?)(.+?)(?=&|\Z)|(?<=&)(.+?)(?=&)|(?<=&)(.+?)(?=\Z)')
+        video_params = regex_pattern.findall(f'{url}')
+        # Flattens the regex result and removes empty string
+        cleaned_video_params = list(filter(None, [item for sublist in video_params for item in sublist]))
+
+        video_params_dict = {}
+        for param in cleaned_video_params:
+            param_pattern = re.compile(r'(.*=)')
+            param_id_pattern = re.compile(r'=(.*)')
+
+            param_type = param_pattern.search(param)
+            param_id = param_id_pattern.search(param)
+
+            cleaned_param_type = param_type.group()
+            cleaned_param_id = param_id.group(1)
+
+            video_params_dict[cleaned_param_type] = video_params_dict.get(cleaned_param_type, cleaned_param_id)
+
+        return video_params_dict
