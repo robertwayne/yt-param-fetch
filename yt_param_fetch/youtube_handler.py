be6b929f22f904d53e84ce7d4c5bfe3498b3094a
Rob <rob.wagner@outlook.com>
2019-01-23 02:45:13 -0500

    New regex and logic for handling non-standard links

--- yt_param_fetch/youtube_handler.py
+++ yt_param_fetch/youtube_handler.py
@@ -9,21 +9,26 @@ class YoutubeHandler:
         Keyword arguments:
             url -- a youtube url
         """
-        regex_pattern = re.compile(r'(?<=\?)(.+?)(?=&|\Z)|(?<=&)(.+?)(?=&)|(?<=/v/)(.+?)(?=\?|\Z)|(?<=&)(.*)(?!=\Z)|(?<=\.be/)(.+?)(?=\?)|(?<=\?)(.+?)(?=\?|\Z)|(?<=\?)(.*)(?!=\Z)|(?<=\.be/)(.*)')
+        regex_pattern = re.compile(r'(?<=\?)(.+?)(?=&|\Z)|(?<=&)(.+?)(?=&)|(?<=/vi/)(.+?)(?=\?|\Z)|(?<=/v/)(.+?)(?=\?|\Z)|(?<=\&)(.*)(?!=\Z)|(?<=\.be/)(.?)(?=&)|(?<=\.be/)(.+?)(?=\?)|(?<=\?)(.+?)(?=\?|\Z)|(?<=\?)(.*)(?!=\Z)|(?<=\.be/)(.*)')
         video_params = regex_pattern.findall(f'{url}')
         # Flattens the regex result and removes empty string
         cleaned_video_params = list(filter(None, [item for sublist in video_params for item in sublist]))
 
         video_params_dict = {}
         for param in cleaned_video_params:
-            param_pattern = re.compile(r'(.*=)')
+            param_pattern = re.compile(r'(.+)(?<=/)|(?!==)(.*=)')
             param_id_pattern = re.compile(r'=(.*)')
 
             param_type = param_pattern.search(param)
             param_id = param_id_pattern.search(param)
 
-            cleaned_param_type = param_type.group()
-            cleaned_param_id = param_id.group(1)
+            # non-standard URL's using /v/, /vi/ or
+            if param_type is None or param_id is None:
+                cleaned_param_type = 'v='
+                cleaned_param_id = param
+            else:
+                cleaned_param_type = param_type.group()
+                cleaned_param_id = param_id.group(1)
 
             video_params_dict[cleaned_param_type] = video_params_dict.get(cleaned_param_type, cleaned_param_id)
 
